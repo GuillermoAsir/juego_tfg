db56908c4842269b2f6462cc72ce3d7f5b827eb9
Guillermo <2820945@alu.murciaeduca.es>
2025-05-26 20:32:13 +0200

    sin mapa

--- dialogos.gd
+++ dialogos.gd
@@ -12,7 +12,7 @@ var mision_inicial_dialogs2 = [
 	"¡Perfecto! Has encontrado el archivo `IPS_Departamentos.txt`.\n" +
 	"Ahora falta un paso más: ver el contenido de dicho fichero.\n" +
 	"Para ello vas a usar el comando `cat` seguido del nombre del fichero,\n" +
-	"por ejemplo: `cat IPS_El_Bohío.txt`. ¡Vamos, usa a ese gatito!"
+	"por ejemplo: `cat IPS_El_Bohío.txt`. ¡Vamos, usa a ese gatito!!"
 ]
 
 var mision_inicial_dialogs3 = [
